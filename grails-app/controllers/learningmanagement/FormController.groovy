a2d8c9305b60b405290f354c6e7038c57547160e
Atul Aggarwal <atulaggarwal12594@gmail.com>
2016-06-10 14:12:11 +0530

    Added to grails

--- grails-app/controllers/learningmanagement/FormController.groovy
+++ grails-app/controllers/learningmanagement/FormController.groovy
@@ -6,6 +6,11 @@ class FormController {
         println("Success")
         println(params)
         render(params)
+        redirect(controller: "form" , action: "thankyou" , params : [firstName : "${params.firstName}"])
+    }
+    def thankyou() {
+
+        render("page redirected ${params.firstName}")
     }
 }
 
