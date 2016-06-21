2832e0b9cf6274f7eefd60c44f1266c58b3e8799
Atul Aggarwal <atulaggarwal12594@gmail.com>
2016-06-21 14:15:12 +0530

    Added to grails criteria modified

--- grails-app/controllers/learningmanagement/UserController.groovy
+++ grails-app/controllers/learningmanagement/UserController.groovy
@@ -3,74 +3,98 @@ package learningmanagement
 class UserController {
 
     def create() {
-       // [myCurrentPage : "Create"]
-        [myuser:new Person()] /*to avoid null pointer exception an empty instance is created*/
+        // [myCurrentPage : "Create"]
+        [myuser: new Person()] /*to avoid null pointer exception an empty instance is created*/
     }
+
     def save() {
-       /* if(!session.count)
+        /* if(!session.count)
         {
            session.count = 0
         }
         session.count  = session.count + 1*/
-       // params.age = params.age.toInteger();
+        // params.age = params.age.toInteger();
         /*User1 u1 = new  User1 ([myfirstName : params.firstName, mylastName : params.lastName, myemail : params
 .emailId, myage: params.age, id: session.count])*/
-       // u1.my()
-       Person myuser = new Person([firstName : params.firstname , lastName : params.lastname, email : params.emailid,
-                                   age : params.myage])
+        // u1.my()
+        Person myuser = new Person([firstName: params.firstname, lastName: params.lastname, email: params.emailid,
+                                    age: params.myage])
 
         myuser.save()
         print myuser.errors
 
-        if(myuser.hasErrors()) {
-            render(view : "create", model: [myuser: myuser])
+        if (myuser.hasErrors()) {
+            render(view: "create", model: [myuser: myuser])
             return
         }
 
-            redirect(action : "list", id : myuser.id)
+        redirect(action: "list", id: myuser.id)
 
     }
+
     def edit() {
-        return [edit1 : Person.get(params.id)]
+        return [edit1: Person.get(params.id)]
     }
+
     def show() {
         Person myperson = Person.get(params.id)
-        [recentusers : myperson, myCurrentPage : "Show"]
+        [recentusers: myperson, myCurrentPage: "Show"]
     }
 
     def list() {
-        [allusers : Person.list(), myCurrentPage : "List"]
+        [allusers: Person.list(), myCurrentPage: "List"]
 
     }
+
     def update() {
-        println ("abc")
+        println("abc")
         println params
         Person myperson = Person.get(params.id)
 
-        myperson.firstName=params.firstname
-        myperson.lastName=params.lastname
-        myperson.email=params.emailid
-        myperson.age=params.int("myage")
+        myperson.firstName = params.firstname
+        myperson.lastName = params.lastname
+        myperson.email = params.emailid
+        myperson.age = params.int("myage")
 
 
         myperson.save()
-        redirect(action:'list')
+        redirect(action: 'list')
 
     }
+
     def delete() {
         Person personInstance = Person.get(params.id)
         personInstance.delete()
 
-        redirect(action:'list')
+        redirect(action: 'list')
     }
+
     def search() {
 
     }
-    def test() {
-        int value = params.int('selection')
-        println value
 
-        if(value == 1) {
+    def test() {
+
+        int value = params.int('selection')
+
+        def a = Person.createCriteria()
+        def result =a.list {
+
+            like('email', "%${params.text1}%")
+
+            if (value == 1) {
+                gt("age", params.int('text2'))
+            } else if (value == 2) {
+                eq("age", params.int('text2'))
+            } else if (value == 3) {
+                lt("age", params.int('text2'))
+            }
+            
+        }
+        render(view:"search", model:[personInst:result])
+    }
+
+        /*   if(value==1) {
 
             List my1 = Person.findAllByEmailLikeAndAgeGreaterThan("%${params.text1}%",params.int ('text2'))
             println my1
@@ -86,7 +110,13 @@ class UserController {
             println my1
             render(view : "search" , model:[personInst:my1])
         }
-
+        def result=Person.createCriteria().list {
+            projections {
+                count()
+            }
+        }
+        println "the number of rows is ${result[0]}"
         //render(view : "search" , model:[personInst:my1])
-    }
+    }*/
 }
+
--- grails-app/views/user/search.gsp
+++ grails-app/views/user/search.gsp
@@ -19,9 +19,18 @@
                 border:1px solid black ;
                 width: 50%;
                 margin: auto;
-                padding: 4%;
+                padding: 2%;
                 background-color:#337ab7;
                 border-radius: 20px;
+            }
+            .box1 {
+                border:1px solid black ;
+                border-radius: 20px;
+                background-color:slategray;
+                width:50%;
+                margin:50px 0px 0px 280px;
+                padding: 2%;
+                color:#ffffff;
             }
         </style>
 </head>
@@ -32,38 +41,40 @@
             <div class="form-group form-group-lg">
                 <label for = "text1" class="col-sm-3 control-label">Email</label>
                 <div class="col-sm-8">
-                    <g:textField name="text1" class="form-control" style="padding:5px; margin:5px;"/><br>
+                    <g:textField name="text1" class="form-control" style="padding:2px; margin:2px;"/>
                 </div>
             </div>
 
             <div class="form-group form-group-lg">
                 <label for="text2" class="col-sm-3 control-label">Age</label>
                 <div class="col-sm-8">
-                    <g:textField  name="text2" class="form-control" style="padding:5px; margin:5px;"/><br>
+                    <g:textField  name="text2" class="form-control" style="padding:2px; margin:2px;"/>
                 </div>
             </div>
             <div class="form-group form-group-lg">
             <label for="selection" class="col-sm-3 control-label">Condition</label>
-                <div class="col-sm-8">
-                    <g:select name = "selection" from ="${[[abc: "Greater",xyz:1],[abc:"Equal",xyz:2],[abc : "Less", xyz : 3]]}"
+                <div class="col-sm-6">
+                    <g:select name = "selection" class="form-control" from ="${[[abc: "Greater",xyz:1],[abc:"Equal",xyz:2],[abc :
+                                                                                                                    "Less", xyz : 3]]}"
                           noSelection="['':'SELECT' ]" onchange="${remoteFunction(params : 'this.value')}"
-                          optionValue="abc" optionKey="xyz"  style="padding:5px; margin:5px;"/><br>
+                          optionValue="abc" optionKey="xyz"  style="padding:2px; margin:2px;"/>
                 </div>
             </div>
             <div class="form-group form-group-lg">
-               <div class="col-sm-8 col-sm-offset-3">
+               <div class="col-sm-6 col-sm-offset-3">
                    <g:submitButton name="Submit" class = "btn btn-lg btn-block btn-success"/>
                </div>
             </div>
         </g:form>
     </div>
+
+    <div class="box1">
+        <g:if test = "${personInst}">
+            Found <br>
+            Name : ${personInst.firstName}
+
+        </g:if>
+    </div>
 </div>
-<g:if test = "${personInst}">
-    Found <br>
-    Name : ${personInst.firstName}
-
-</g:if>
-
-
 </body>
 </html>
